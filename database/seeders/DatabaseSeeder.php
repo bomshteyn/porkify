bc0b93fa2c4b0bb3eef8e515106a33563ec7294f
Nathan <baumnathan@gmail.com>
2023-01-10 15:06:34 -0500

    don't seed songs and tags

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-        $this->call(SongSeeder::class);
-        $this->call(TagSeeder::class);
+        \App\Models\User::factory(1)->create();
+//        $this->call(SongSeeder::class);
+//        $this->call(TagSeeder::class);
     }
 }
